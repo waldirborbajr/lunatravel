7aa05ff17d26a2310586ffd39826d79a792c572b
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-21 15:42:44 -0300

    feat: add input field
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- pages/bank/index.vue
+++ pages/bank/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <NuxtLink to="/">Home</NuxtLink>
-  <div>Bank</div>
+  <div id="bank" class="mt-6 w-full bg-yellow-400">
+    <div class="mx-auto max-w-lg">
+      <div class="py-1">
+        <span class="px-1 text-sm text-gray-600">Bank Name</span>
+        <input placeholder="" type="text"
+          class="block py-2 px-3 w-full placeholder-gray-600 bg-white rounded-lg border-2 border-gray-300 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none text-md" />
+      </div>
+      <div class="py-1">
+        <span class="px-1 text-sm text-gray-600">Agency</span>
+        <input placeholder="" type="text"
+          class="block py-2 px-3 w-full placeholder-gray-600 bg-white rounded-lg border-2 border-gray-300 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none text-md" />
+      </div>
+      <div class="py-1">
+        <span class="px-1 text-sm text-gray-600">Account Number</span>
+        <input placeholder="" type="text"
+          class="block py-2 px-3 w-full placeholder-gray-600 bg-white rounded-lg border-2 border-gray-300 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none text-md" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 useHead({
-  title: "Bank",
+  title: "Bank Management",
 });
 
 const { consLog } = useUtils();
