5b16559f5d0c2932666b2747b746278dfc268511
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-05-21 14:59:09 -0300

    feat: splitted menu as component
    Signed-off-by: Waldir BORBA JUNIOR <wborbajr@gmail.com>

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -5,32 +5,7 @@
         <Icon name="material-symbols:currency-exchange" size="24" />
         Luna Travel Monetary Services
       </div>
-      <div class="hidden justify-center items-center pt-1 antialiased lg:flex lg:ml-20">
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/customer">Customer</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/bank">Bank</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/city">City</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/identification">Identification</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/parameter">Parameter</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/reports">Parameter</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/user">User</NuxtLink>
-        <NuxtLink
-          class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
-          to="/login">Logout</NuxtLink>
-      </div>
+      <Menu />
     </div>
   </div>
 
---
+++ components/Menu/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hidden justify-center items-center pt-1 antialiased lg:flex lg:ml-20">
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/customer">Customer</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/bank">Bank</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/city">City</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/identification">Identification</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/parameter">Parameter</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/reports">Reports</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/user">User</NuxtLink>
+    <NuxtLink
+      class="flex justify-center items-center mr-10 text-base font-medium tracking-tight text-gray-700 text-opacity-90 transition duration-150 ease-in-out hover:text-cool-gray-600"
+      to="/login">Logout</NuxtLink>
+  </div>
+</template>
